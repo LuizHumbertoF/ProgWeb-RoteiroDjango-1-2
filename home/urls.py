5161c159501038f9ecb2ad50886a7fb6a77f01a5
luizhumberto <luizhumbertofn@gmail.com>
2026-06-29 11:37:45 -0300

    parte 2 finalizada

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("sobre/", views.sobre, name="sobre"),       # ← nova rota
 ]
